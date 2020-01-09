ccfe1ccf228adb6640325aa811a1f683876f1f6d
rgiron1 <rgiro004@hammer.cs.ucr.edu>
2020-01-08 23:27:58 -0800

    "c-echo.cpp"

--- c-echo.cpp
+++ c-echo.cpp
@@ -3,7 +3,10 @@
 int main(int argv, char** argc) {
     // Skip the first argc index because its the program
          for(int i = 1; i < argv; i++) {
-                 std::cout << argc[i] << " ";
+                 std::cout << argc[i];
+		if (i < argv - 1) {
+			std::cout << " ";
+			}
                      }
                          std::cout << std::endl;
-                         }
+                }
